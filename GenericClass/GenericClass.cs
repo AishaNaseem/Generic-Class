9b4d0b6bbb72945a17b48bd70828cce691015dde
AishaNaseem <aishanaseemj@gmail.com>
2023-04-18 20:54:34 -0400

    Add files via upload

---
+++ GenericClass/GenericClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class GenericClass<T> where T : struct
+    {
+
+        private List<T> list = new List<T>();
+     
+       
+        public void AddList(T item)
+        {
+            list.Add(item);
+        }
+        public T showdata(int itemIndex)
+        {
+
+                return list[itemIndex];
+          } 
+
+        public List<T> getSortList()
+        {
+            list.Sort();
+            return list;
+        }
+    }
+}
---
+++ GenericClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericClass;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        GenericClass<int> gn = new GenericClass<int> ();
+        gn.AddList(2);
+        gn.AddList(10);
+        gn.AddList(4);
+        gn.AddList (5);
+        var result=gn.showdata(2);
+        Console.WriteLine(result);
+        result= gn.showdata(3);
+        Console.WriteLine(result);
+        var ordered = gn.getSortList();
+            foreach(var i in ordered)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
